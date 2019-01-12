74fc43ca3cafd26283b4205db0be44f9917cace5
Kajornphat Ngamsom <s5901012620029@email.kmutnb.ac.th>
2019-01-13 00:06:48 +0700

    reader() change to select calculateGPA(semester)

--- main.py
+++ main.py
@@ -22,11 +22,18 @@ def insert():
 	recent_input = 'At semester '+semester+' you attend '+subject+'which has '+credit+' credits and you got '+grade+', has been added to csv files'
 	print(recent_input)
 
-def reader():
+def calculateGPA(semester):
+	grade = {'A': 4, 'B+': 3.5, 'B': 3, 'C+': 2.5, 'C': 2, 'D+': 1.5, 'D': 1, 'F': 0}
+	sum_point = 0
+	sum_credit = 0
 	global file
 	with open(file, newline='') as csvfile:
 		csv_reader = csv.DictReader(csvfile)
 		for row in csv_reader:
-			print(row)
+			if row['Semester'] == str(semester):
+				point = int(row['Credit']) * grade[row['Grade']]
+				sum_point += point
+				sum_credit += int(row['Credit'])
+		return sum_point/sum_credit
 
-insert()
+print(calculateGPA(2))
